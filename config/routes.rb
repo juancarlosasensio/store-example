cb1c4954e8c4d879827260151002e96f6e4ff0ce
Juan Carlos Asensio <jasensio@ufm.edu>
2025-10-13 09:29:14 -0400

    Add product in stock mailer

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,10 @@ Rails.application.routes.draw do
   root "products#index"
 
   resources :products
+
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
+
+  resource :unsubscribe, only: [ :show ]
 end
